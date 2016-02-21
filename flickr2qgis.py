cc1916d2e9dbb01b7f43767df159efc8110f277d
riccardoklinger <riccardo.klinger@geolicious.de>
2016-02-21 19:31:01 +0100

    first functional plugin

--- flickr2qgis.py
+++ flickr2qgis.py
@@ -30,11 +30,10 @@ from qgis.gui import *
 import resources
 # Import the code for the dialog
 from flickr2qgis_dialog import flickr2qgisDialog
-import os.path
 import flickrapi
 from qgis.gui import * #for the click event tool
 from xml.etree import ElementTree
-import urllib2, os, qgis.utils, os.path
+import urllib2, os, qgis.utils, os.path, tempfile, urllib, requests, base64
 from datetime import datetime, timedelta
 
 class flickr2qgis:
@@ -80,6 +79,9 @@ class flickr2qgis:
         self.dlg.units.clear()
         self.dlg.units.addItem("km")
         self.dlg.units.addItem("miles")
+        self.dlg.download.clear()
+        self.dlg.download.addItem("download thumbs to tmp")
+        self.dlg.download.addItem("download thumbs to folder")
         self.dlg.enddate.setDate(QDate.currentDate())
         today=QDate.currentDate()
         self.dlg.startdate.setDate(today.addDays(-14))
@@ -196,8 +198,12 @@ class flickr2qgis:
         result = QObject.connect(self.clickTool, SIGNAL("canvasClicked(const QgsPoint &, Qt::MouseButton)"), self.handleMouseDown)
     def handleMouseDown(self, point, button):
         test=self.dlg.address.text()
-        self.dlg.lat.setValue(point.y())
-        self.dlg.lon.setValue(point.x())
+        crsSrc = qgis.utils.iface.mapCanvas().mapSettings().destinationCrs()
+        crsDest = QgsCoordinateReferenceSystem(4326)
+        xform = QgsCoordinateTransform(crsSrc, crsDest)
+        pointwgs84 = xform.transform(point)
+        self.dlg.lat.setValue(pointwgs84.y())
+        self.dlg.lon.setValue(pointwgs84.x())
 
         #self.dlg.setTextBrowser( str(point.x()) + " , " +str(point.y()) )
 
@@ -233,6 +239,8 @@ class flickr2qgis:
             numberOfHits_start = child.attrib["numberOfGeocodedAddresses"]
         if numberOfHits_start != "0":
             start_point=xml[1][0][0][0][0][0].text
+            self.dlg.lon.setValue(float(start_point.split(" ")[0]))
+            self.dlg.lat.setValue(float(start_point.split(" ")[1]))
         if start_point =="":
             QtGui.QMessageBox.about(self.dlg, "No Coordinates Found", "Check your start address!")
 
@@ -282,9 +290,11 @@ class flickr2qgis:
             if len(json['photos']['photo'])>0:
 #as we have all photos, let's add some data to each of it.
                 print json['photos']['photo'][0]
-                layer = QgsVectorLayer('Point?crs=EPSG:4326', 'Accessibility', "memory")
+                layer = QgsVectorLayer('Point?crs=EPSG:4326', 'Flickr', "memory")
                 pr = layer.dataProvider()
                 pr.addAttributes([QgsField("thumbnail", QVariant.String)])
+                pr.addAttributes([QgsField("photoid", QVariant.String)])
+
                 pr.addAttributes([QgsField("title", QVariant.String)])
                 pr.addAttributes([QgsField("owner", QVariant.String)])
                 pr.addAttributes([QgsField("link", QVariant.String)])
@@ -297,30 +307,94 @@ class flickr2qgis:
                 pr.addAttributes([QgsField("locality", QVariant.String)])
                 pr.addAttributes([QgsField("posted", QVariant.String)])
                 pr.addAttributes([QgsField("taken", QVariant.String)])
-
+                
                 layer.updateFields()
+                categories = []
+                renderer = QgsCategorizedSymbolRendererV2("photoid", categories)
+                layer.setRendererV2(renderer)
+                QgsMapLayerRegistry.instance().addMapLayer(layer)
+                #sym.setColor(QColor(value))
+
                 for photo in json['photos']['photo']:
                     fet = QgsFeature()
                 #now read additional information:
                     details = flickr.photos.getInfo(photo_id=photo['id'])
                     thumb = 'https://farm' + str(photo['farm']) + '.staticflickr.com/' + str(photo['server']) + '/' + str(photo['id'])+ '_' + str(photo['secret'])+ '_t.jpg'
-                    taken = details['photo']['dates']['taken']
-                    posted = details['photo']['dates']['posted']
-                    desc = details['photo']['description']['_content']
-                    latitude = details['photo']['location']['latitude']
-                    longitude = details['photo']['location']['longitude']
-                    country = details['photo']['location']['country']['_content']
-                    county = details['photo']['location']['county']['_content']
-                    region = details['photo']['location']['region']['_content']
-                    locality = details['photo']['location']['locality']['_content']
-                    owner =  details['photo']['owner']['username']
-                    title =  details['photo']['title']['_content']
-                    link = 'https://farm' + str(photo['farm']) + '.staticflickr.com/' + str(photo['server']) + '/' + str(photo['id'])+ '_' + str(photo['secret'])+ '_b.jpg'
+                    photoid = str(photo['id'])
+                    try: taken = details['photo']['dates']['taken']
+                    except: taken =""
+                    try: posted = details['photo']['dates']['posted']
+                    except: posted = ""
+                    try: desc = details['photo']['description']['_content']
+                    except: desc =""
+                    try: latitude = details['photo']['location']['latitude']
+                    except: latitude = ""
+                    try: longitude = details['photo']['location']['longitude']
+                    except: longitude =""
+                    try: country = details['photo']['location']['country']['_content']
+                    except: country =""
+                    try: county = details['photo']['location']['county']['_content']
+                    except: county = ""
+                    try: region = details['photo']['location']['region']['_content']
+                    except: region = ""
+                    try: locality = details['photo']['location']['locality']['_content']
+                    except: locality = ""
+                    try: owner =  details['photo']['owner']['username']
+                    except: owner = ""
+                    try: title =  details['photo']['title']['_content']
+                    except: title = ""
+                    try: link = 'https://farm' + str(photo['farm']) + '.staticflickr.com/' + str(photo['server']) + '/' + str(photo['id'])+ '_' + str(photo['secret'])+ '_b.jpg'
+                    except: link = ""
                     print([thumb, title, owner, link, desc, latitude, longitude, country, county, region, locality, posted, taken])
                     fet.setGeometry(QgsGeometry.fromPoint(QgsPoint(float(details['photo']['location']['longitude']),float(details['photo']['location']['latitude']))))
                     geom = fet.geometry()
-                    fet.setAttributes([thumb, title, owner, link, desc, latitude, longitude, country, county, region, locality, posted, taken])
+                    fet.setAttributes([thumb,photoid, title, owner, link, desc, latitude, longitude, country, county, region, locality, posted, taken])
                     pr.addFeatures([fet])
+                    #sym = QgsSymbolV2.defaultSymbol(layer.geometryType())
+                    #sym = QgsSymbolV2(QGis.Point)
+                    #sym.setNamedPointSymbol("svg:/tmp/25059331595_t.jpg.svg")
+                    #sym.setPointSize(30)
+                    #symbollist = layer.rendererV2().symbols()
+                    #symbol = symbollist[0]
+                    #symbol.appendSymbolLayer(sl)
+                    #self.canvas.refresh()  
+
+                    #äsym.changeSymbolLayer(0,QgsSvgMarkerSymbolLayerV2(size=30).setPath("/tmp/25059331595_t.jpg.svg"))
+
+                    #sym.appendSymbolLayer(QgsSvgMarkerSymbolLayerV2(size=30).setPath("/tmp/25059331595_t.jpg.svg"))
+                    #sym.deleteSymbolLayer(0)
+                    if self.dlg.download.currentText()=="download thumbs to tmp":
+                        svg = """
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg>
+  <g>
+    <image xlink:href="data:image/jpeg;base64,{0}" height="256" width="320" />
+  </g>
+</svg>
+"""
+                        
+                        data2 = requests.get(thumb, stream=True).content
+                        nameim = str(photo['id'])+'_t.jpg'
+                        b64response = base64.b64encode(data2)
+                        newsvg = svg.format(b64response).replace('\n','')
+                        path = tempfile.gettempdir() + os.sep + photo['id'] + '_t.jpg.svg'
+                        with open(path, 'w') as f:
+                            f.write(newsvg)
+                    svgStyle = {}
+                    svgStyle['fill'] = '#0000ff'
+                    svgStyle['name'] = tempfile.gettempdir() + os.sep + photo['id'] + '_t.jpg.svg'
+                    svgStyle['outline'] = '#000000'
+                    svgStyle['size'] = '30'
+                    symLyr1 = QgsSvgMarkerSymbolLayerV2.create(svgStyle)
+                    print symLyr1
+                    sym = QgsSymbolV2.defaultSymbol(layer.geometryType())
+                    sym.changeSymbolLayer(0, symLyr1)
+                    category = QgsRendererCategoryV2(str(photo['id']), sym, str(photo['id']))
+                    categories.append(category)
+                    renderer = QgsCategorizedSymbolRendererV2("photoid", categories)
+                    layer.setRendererV2(renderer)
+                    layer.updateExtents()
+                    layer.triggerRepaint()
 
                 QgsVectorFileWriter.writeAsVectorFormat(layer, "/tmp/access_berlin.shp", "CP1250", None, "ESRI Shapefile")
 
